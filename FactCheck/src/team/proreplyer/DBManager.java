0bcd7c382c28d940d9559d9ce4f6b047053ecbdd
Seongjun2 <tjdwns4302@naver.com>
2018-10-10 22:45:35 +0900

    환경설정.

--- FactCheck/src/team/proreplyer/DBManager.java
+++ FactCheck/src/team/proreplyer/DBManager.java
@@ -14,7 +14,7 @@ public class DBManager {
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/factcheck?characterEncoding=UTF-8&serverTimezone=UTC";
 	private static final String USER = "root";
-	private static final String PASS = "root1";
+	private static final String PASS = "seongjun100";
 
 	private static Connection conn = null;
 
